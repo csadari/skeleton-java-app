49252d0a1469d3272da72105ca43322418835149
csadari <adarics@gmail.com>
2020-06-24 10:41:28 -0400

    Push services

--- src/main/java/com/develogical/QueryProcessor.java
+++ src/main/java/com/develogical/QueryProcessor.java
@@ -6,10 +6,23 @@ public class QueryProcessor {
         if (query.toLowerCase().contains("romeo and juliet")) {
             return "**** William Shakespeare **** ";
         }
+
         else if (query.toLowerCase().contains("what is your name")) {
             return "**** Chandra Adari **** ";
         }
+        else if (query.toLowerCase().contains("what is 18 plus 2")) {
+            return "20";
+        }
+        else if (query.toLowerCase().contains("what is 3 plus 6")) {
+            return "9";
+        }
+        else if (query.toLowerCase().contains("which of the following numbers is the largest:78, 62, 46, 873")) {
+            return "873";
+        }
         else
         return "";
+
+
     }
+
 }
--- src/test/java/com/develogical/QueryProcessorTest.java
+++ src/test/java/com/develogical/QueryProcessorTest.java
@@ -28,4 +28,14 @@ public class QueryProcessorTest {
   public void returnsEmptyStringIfCannotProcessQuery() throws Exception {
     assertThat(queryProcessor.process("test"), is(""));
   }
+
+  @Test
+  public void returnsNumberString() throws Exception {
+    assertThat(queryProcessor.process("what is 18 plus 2"), containsString("20"));
+  }
+
+
+
+
+
 }
